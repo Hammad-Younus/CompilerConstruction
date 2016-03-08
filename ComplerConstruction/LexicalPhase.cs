f1af4b586a294899e21f5fad655eedc4da403763
Hammad <hammadyounus1234@hotmail.com>
2016-03-09 00:26:27 +0500

    Starting Lexical Phase

---
+++ ComplerConstruction/LexicalPhase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CompilerConstruction
+{
+    class LexicalPhase
+    {
+        int i;
+        public void Write()
+        {
+            Console.WriteLine("My Name Is Hammad");
+        }
+    }
+}
