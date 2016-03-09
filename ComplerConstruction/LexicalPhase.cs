2c68618a03519fdc4c1c30a5bcfe43f6d0257575
Hammad <hammadyounus1234@hotmail.com>
2016-03-09 14:41:16 +0500

    Spliting Keywords and Punctuators

--- ComplerConstruction/LexicalPhase.cs
+++ ComplerConstruction/LexicalPhase.cs
@@ -8,17 +8,32 @@ namespace CompilerConstruction
 {
     class LexicalPhase
     {
+        public string[] keywords = new string[] {"bold","div","alpha","bit","twine","stop","current","obj","def","empty","begin","Continue","Either","Or","Or Either","At","Proc","Allign","Template"};
+        public string[] Punctuators = new string[] {"(", ")", "{", "}",".."};
+        public string WordBreaker = ",.:(){}='..' ";
         public string[] classPart;
         public string[] valuePart;
+        string[] split = null;
         int lineNo = 0;
-        public string[] Code;
 
         public void ReadFile()
         {
             string text;
             using (StreamReader sr = new StreamReader(@"D:\CC_Work\CompilerConstruction\Files\Main.txt"))
             {
-                text = sr.ReadToEnd();
+                char[] Delimeters = WordBreaker.ToCharArray();
+                do
+                {
+                   text = sr.ReadLine();
+                   lineNo++;
+                   int i = lineNo;
+                   split = text.Split(Delimeters,Delimeters.Length);
+                   Console.WriteLine("\nLine Number = {0} ",i);
+	               foreach (string s in split) 
+                   {
+                        Console.WriteLine("Keyword = {0} ", s);
+  	               }
+                }while(text != null);
             }
         }
     }
